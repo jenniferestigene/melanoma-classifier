80db05a820dca946ddbda2b32397322f97728b39
jenniferestigene <235264@students.hertie-school.org>
2026-08-20 17:56:29 -0400

    updates to CNN

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
-from torchvision import models
+import torch.nn.functional as F
+
+
+img_size = 100
 
 
 class Net(nn.Module):
@@ -8,14 +11,22 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # Load ResNet18 with weights pretrained on ImageNet (1.2M+ images, 1000 classes)
-        self.resnet = models.resnet18(weights='IMAGENET1K_V1')
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5)
 
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-            
-        num_features = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_features, 2)
+        self.fc1 = nn.Linear(128*9*9, 512)
+        self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
-        return self.resnet(x)
+
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
+
+        x = x.view(-1, 128*9*9)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
